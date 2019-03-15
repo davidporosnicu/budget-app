4f7a04d9a6bd14712dde0afa3e7e3fcddefe0796
David Porosnicu <david94_p@yahoo.com>
2019-03-15 11:09:03 +0200

    initial boilerplate

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import NotFoundPage from "./components/NotFoundPage";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" />
+      <Route path="/create" />
+      <Route path="/edit" />
+      <Route path="/help" />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </BrowserRouter>
+);
+ReactDOM.render(routes, document.querySelector("#root"));
