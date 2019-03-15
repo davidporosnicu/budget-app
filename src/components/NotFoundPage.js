bca9dca0cdad727c2e03a453108f2bd98053250f
David Porosnicu <david94_p@yahoo.com>
2019-03-15 11:29:46 +0200

    add NotFound Comp + header

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function NotFoundPage() {
-  return <div>404!</div>;
+  return (
+    <div>
+      404! <Link to="/"> Go Home</Link>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 import NotFoundPage from "./components/NotFoundPage";
+import "./styles/base/_base.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Expensify</h1>
+      <NavLink to="/" exact activeClassName="is-active">
+        Dashboard
+      </NavLink>
+      <NavLink to="/create" activeClassName="is-active">
+        Create Expense
+      </NavLink>
+      <NavLink to="/edit" activeClassName="is-active">
+        Edit Expense
+      </NavLink>
+      <NavLink to="/help" activeClassName="is-active">
+        Help
+      </NavLink>
+    </header>
+  );
+};
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" />
-      <Route path="/create" />
-      <Route path="/edit" />
-      <Route path="/help" />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" />
+        <Route path="/create" />
+        <Route path="/edit" />
+        <Route path="/help" />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   </BrowserRouter>
 );
 ReactDOM.render(routes, document.querySelector("#root"));
